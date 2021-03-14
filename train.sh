1357679931cd5f57e906bc78a29172f2cefbaa9c
qilei123 <qileimail123@gmail.com>
2021-03-14 16:50:24 -0400

    test

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --coco_path /data1/qilei_chen/DATA/erosive --output_dir /data1/qilei_chen/DATA/erosive/detr_models1
+python -m torch.distributed.launch --nproc_per_node=4 --master_port 9898 --use_env main.py --coco_path /data1/qilei_chen/DATA/erosive --output_dir /data1/qilei_chen/DATA/erosive/detr_models1
