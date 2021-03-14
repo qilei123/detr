d227e865b2c6d6db12bcb8619d83415849b4785c
qilei123 <qileimail123@gmail.com>
2021-03-14 00:52:01 -0500

    test

--- models/detr.py
+++ models/detr.py
@@ -317,6 +317,8 @@ def build(args):
         # for panoptic, we just add a num_classes that is large enough to hold
         # max_obj_id + 1, but the exact value doesn't really matter
         num_classes = 250
+    print(num_classes)
+    num_classes = 2
     device = torch.device(args.device)
 
     backbone = build_backbone(args)
