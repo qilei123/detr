298585121b7bf491b0bdd3127f70dbf3eebe80b7
qilei123 <qileimail123@gmail.com>
2021-03-14 00:54:31 -0500

    test

--- models/detr.py
+++ models/detr.py
@@ -123,6 +123,8 @@ class SetCriterion(nn.Module):
 
         if log:
             # TODO this should probably be a separate loss, not hacked in this one here
+            print(src_logits[idx])
+            print(target_classes_o)
             losses['class_error'] = 100 - accuracy(src_logits[idx], target_classes_o)[0]
         return losses
 
